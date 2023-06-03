690fdf9d7ab938ff8338f8eb611ff3004ee2dd2a
bruh.ngl0 <adityashrm500@gmail.com>
2023-06-03 22:18:55 +0530

    ui

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Login = () => {
+
+  const [formData , setFormData] = useState({
+  
+    email: "",
+    password: "",
+   
+  })
+
+
+  const { email, password } = formData
+
+  const onChange = (e)=> {
+    setFormData((prevState)=> ({
+      ...prevState,
+      [e.target.name]: e.target.value, 
+    }))
+
+  }
+
+  const onSubmit = (e)=> {
+    e.preventDefault()
+
+    //dispatch
+  }
+
   return (
-    <div className='login-main'>
-      login
+    <div className='register-main'>
+      <h1>Login to set goals</h1>
+      <form onSubmit={onSubmit}>
+
+
+        <input
+         id='email'
+         placeholder='email'
+         name='email'
+         value= {email}
+         onChange={onChange}>          
+         </input>
+
+        <input
+         id='password'
+         placeholder='password'
+         name='password'
+         value= {password}
+         onChange={onChange}>
+         </input>
+
+        <button type = "submit">
+          submit
+        </button>
+
+      </form>
     </div>
   )
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Register = () => {
+
+  const [formData , setFormData] = useState({
+    name: '',
+    email: "",
+    password: "",
+    password2: "",
+  })
+
+
+  const {name, email, password, password2} = formData
+
+  const onChange = (e)=> {
+    setFormData((prevState)=> ({
+      ...prevState,
+      [e.target.name]: e.target.value, 
+    }))
+
+  }
+
+  const onSubmit = (e)=> {
+    e.preventDefault()
+
+    //dispatch
+  }
+
   return (
     <div className='register-main'>
-      register
+      <h1>Register User</h1>
+      <form onSubmit={onSubmit}>
+
+        <input
+        id='name'
+        placeholder='name'
+        name='name'
+        value= {name}
+        onChange={onChange}>   
+        </input>
+
+        <input
+         id='email'
+         placeholder='email'
+         name='email'
+         value= {email}
+         onChange={onChange}>          
+         </input>
+
+        <input
+         id='password'
+         placeholder='password'
+         name='password'
+         value= {password}
+         onChange={onChange}>
+         </input>
+
+        <input
+         id='password2'
+         placeholder='confirm password'
+         name='password2'
+         value= {password2}
+         onChange={onChange}>
+         </input>
+
+        <button type = "submit">
+          submit
+        </button>
+
+      </form>
     </div>
   )
 }
